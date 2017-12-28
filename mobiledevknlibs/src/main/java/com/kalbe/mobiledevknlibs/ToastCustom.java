8e48aa8c83d1d4bb7cbc5ac588f9561084650d37
aanjunian <juniantoaan87@gmail.com>
2017-12-28 16:28:34 +0700

    Initial commit

---
+++ mobiledevknlibs/src/main/java/com/kalbe/mobiledevknlibs/ToastCustom.java
@@ -0,0 +1,18 @@
+package com.kalbe.mobiledevknlibs;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.Gravity;
+import android.widget.Toast;
+
+/**
+ * Created by aan.junianto on 28/12/2017.
+ */
+
+public class ToastCustom extends Activity{
+    public static void showToastStatic(Context ctx, String str) {
+        Toast toast = Toast.makeText(ctx, str, Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.TOP, 25, 400);
+        toast.show();
+    }
+}
