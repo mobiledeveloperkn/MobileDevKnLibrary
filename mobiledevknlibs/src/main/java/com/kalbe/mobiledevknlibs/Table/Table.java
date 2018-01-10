e3bdc580311be4f2f192bcb174deaec95aefc636
dewi9310 <dewioktaviani9310@gmail.com>
2018-01-10 13:52:15 +0700

    create custom table

--- mobiledevknlibs/src/main/java/com/kalbe/mobiledevknlibs/Table/Table.java
+++ mobiledevknlibs/src/main/java/com/kalbe/mobiledevknlibs/Table/Table.java
@@ -1,13 +1,80 @@
 package com.kalbe.mobiledevknlibs.Table;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.support.v7.app.AlertDialog;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import com.kalbe.mobiledevknlibs.R;
+
+import java.util.ArrayList;
 
 /**
  * Created by Robert on 04/01/2018.
  */
 
 public class Table {
+    private void setTableLayout(Context context, TableLayout tableLayout, String[] colTextHeader, ArrayList<String[]> colData) {
+//        LayoutInflater inflater = (LayoutInflater) context.getSystemService( Context.LAYOUT_INFLATER_SERVICE );
+//        final View promptView = inflater.inflate(R.layout.popup_table, null);
+//        TableLayout tlb = (TableLayout) promptView.findViewById(R.id.tlProductQty);
+        tableLayout.removeAllViews();
+
+        TableRow.LayoutParams params = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT);
+        params.setMargins(1, 1, 1, 1);
+
+        TableRow tr = new TableRow(context);
+
+        TableLayout tl = new TableLayout(context);
+
+        for (String text : colTextHeader) {
+            TextView tv = new TextView(context);
+            tv.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT, 1));
+
+            tv.setTextSize(14);
+            tv.setPadding(10, 10, 10, 10);
+            tv.setText(text);
+            tv.setGravity(Gravity.CENTER);
+            tv.setBackgroundColor(Color.parseColor("#4CAF50"));
+            tv.setTextColor(Color.WHITE);
+            tr.addView(tv,params);
+        }
+        tl.addView(tr);
+        for(String[] data : colData){
+            tr = new TableRow(context);
+            TableLayout.LayoutParams tableRowParams=
+                    new TableLayout.LayoutParams
+                            (TableLayout.LayoutParams.MATCH_PARENT,TableLayout.LayoutParams.WRAP_CONTENT);
+
+            int leftMargin=0;
+            int topMargin=0;
+            int rightMargin=0;
+            int bottomMargin=0;
+            tableRowParams.setMargins(leftMargin, topMargin, rightMargin, bottomMargin);
+
+            tr.setLayoutParams(tableRowParams);
+            for (int i=0; i < data.length; i++){
+                TextView product = new TextView(context);
+                product.setTextSize(12);
+                product.setWidth(400);
+                product.setPadding(10, 10, 10, 10);
+                product.setBackgroundColor(Color.parseColor("#f0f0f0"));
+                product.setTextColor(Color.BLACK);
+                product.setText(i);
+                tr.addView(product,params);
+            }
+            tl.addView(tr, tableRowParams);
+        }
+
+        tableLayout.addView(tl);
+    }
     public void setTable(Context context, TableLayout tableLayout){{
 
     }
